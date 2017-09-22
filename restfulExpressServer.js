38f25695f51314f9736a1693a8a6640607b16b36
“mandafae“ <mandafae5@gmail.com>
2017-09-22 15:18:18 -0500

    added expressserver and restful

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 const fs = require('fs');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 
 // Create function to read pets file
@@ -19,10 +19,10 @@ function readFile() {
       resolve(JSON.parse(data));
     });
   });
-}
+};
 
 
-// Display all pets
+// Display all pets (GET)
 app.get('/pets', function (req, res) {
   readFile().then((info) => {
       res.statusCode = 200;
@@ -30,12 +30,12 @@ app.get('/pets', function (req, res) {
       res.end(JSON.stringify(info));
     });
   return;
-})
+});
 
 
-// Display pet by ID
+// Display pet by ID (GET :ID)
 app.get('/pets/:id', function(req, res) {
-  var id = parseInt(req.params.id);
+  let id = parseInt(req.params.id);
   readFile().then((info) => {
     if (id < 0 || id >= info.length || isNaN(id)) {
       res.set('Content-Type', 'text/plain').status(404).send('Not Found');
@@ -47,7 +47,7 @@ app.get('/pets/:id', function(req, res) {
 });
 
 
-// POST request
+// Add new pet (POST)
 app.post('/pets', function(req, res) {
   let newPet = req.body;
   if (!newPet || !newPet.name || !newPet.kind || !/^\d*$/.test(newPet.age)) {
@@ -61,6 +61,53 @@ app.post('/pets', function(req, res) {
     res.set('Content-Type', 'application/json').status(200).send(newPet);
   });
 };
+  return;
+});
+
+
+// Update existing pet (PATCH)
+app.patch('/pets/:id', function(req, res) {
+  let update = req.body;
+  let id = parseInt(req.params.id);
+  readFile().then((info) => {
+    if (id < 0 || id >= info.length || isNaN(id)) {
+      res.set('Content-Type', 'text/plain').status(404).send('Not Found');
+    } else if (update.name || update.age || update.kind) {
+      if (update.name) {
+        info[id].name = update.name;
+      }
+      if (update.age) {
+        info[id].age = parseInt(update.age);
+      }
+      if (update.kind) {
+        info[id].kind = update.kind;
+      }
+    } else {
+      res.set('Content-Type', 'text/plain').status(400).send('Bad Request');
+    }
+    fs.writeFile('pets.json', JSON.stringify(info), function(err) {
+      if (err) throw err;
+    })
+    res.set('Content-Type', 'application/json').status(200).send(info[id]);
+  });
+  return;
+});
+
+
+// Delete pet
+app.delete('/pets/:id', function(req, res) {
+  let id = parseInt(req.params.id);
+  readFile().then((info) => {
+    if (id < 0 || id >= info.length || isNaN(id)) {
+      res.set('Content-Type', 'text/plain').status(404).send('Not Found');
+    } else {
+      let removed = info.splice(id, 1);
+      fs.writeFile('pets.json', JSON.stringify(info), function(err) {
+        if (err) throw err;
+      })
+      res.set('Content-Type', 'application/json').status(200).send(removed);
+    };
+  });
   return;
 });
 
